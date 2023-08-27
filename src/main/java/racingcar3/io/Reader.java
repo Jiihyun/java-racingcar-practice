001c58ac11e846d070cac61ce845f58f300edfa2
지현 <cwlgusc@gmail.com>
2023-08-27 21:41:49 +0900

    Feat: read names and try times

--- src/main/java/racingcar3/io/Reader.java
+++ src/main/java/racingcar3/io/Reader.java
@@ -1,7 +1,19 @@
 package racingcar3.io;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar3.model.Car;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Reader {
+    public List<String> readNames() {
+         return Arrays.stream(Console.readLine().split("")).collect(Collectors.toList());
+    }
 
+    public int readTryTimes() {
+        return Integer.parseInt(Console.readLine());
+    }
 }
