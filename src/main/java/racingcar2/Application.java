8b89a609a99f01f471023821f5faa7941a601178
지현 <cwlgusc@gmail.com>
2023-08-30 01:17:08 +0900

    Feat: connect application to controller(racingcar2)

--- src/main/java/racingcar2/Application.java
+++ src/main/java/racingcar2/Application.java
@@ -3,5 +3,8 @@ package racingcar2;
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
+        Container container = new Container();
+        GameController gameController = container.gameController();
+        gameController.start();
     }
 }
