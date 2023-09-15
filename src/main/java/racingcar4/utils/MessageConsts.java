1c4af6ffc65ec852d7179773bc919ec048d3be7a
지현 <cwlgusc@gmail.com>
2023-09-15 23:32:52 +0900

    refactor: add const

--- src/main/java/racingcar4/utils/MessageConsts.java
+++ src/main/java/racingcar4/utils/MessageConsts.java
@@ -5,7 +5,7 @@ public class MessageConsts {
     public static final String INPUT_TRY_TIMES_MSG = "시도할 회수는 몇회인가요?";
     public static final String GAME_RESULT_MSG = "실행 결과";
     public static final String WINNER_MSG = "최종 우승자 : ";
-    public static final String NAME_LENGTH_EXCEPTION = "[ERROR] - 이름은 5자 이하로 입력해주세요.";
+    public static final String NAME_LENGTH_EXCEPTION = "[ERROR] - 이름은 " + NumberConsts.NAME_LIMIT + "자 이하로 입력해주세요.";
     public static final String NAME_DUPLICATE_EXCEPTION = "[ERROR] - 중복된 이름입니다.";
     public static final String TRY_TIMES_TYPE_EXCEPTION = "[ERROR] - 시도 횟수는 숫자여야 합니다.";
 
--- src/main/java/racingcar4/utils/NumberConsts.java
+++ src/main/java/racingcar4/utils/NumberConsts.java
@@ -3,4 +3,5 @@ package racingcar4.utils;
 public class NumberConsts {
     public static int START_INCLUSIVE = 0;
     public static int END_INCLUSIVE = 9;
+    public static int NAME_LIMIT = 5;
 }
