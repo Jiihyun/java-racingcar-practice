7d7c8381cd4d52ce1ca9ae12c74dd4b16523471b
지현 <cwlgusc@gmail.com>
2023-10-03 19:43:56 +0900

    feat: check random number if it is possible to move in MovingPositionGenerator

--- src/main/java/racingcar5/util/MovingPositionGenerator.java
+++ src/main/java/racingcar5/util/MovingPositionGenerator.java
@@ -1,4 +1,12 @@
 package racingcar5.util;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import static racingcar5.util.NumberConst.*;
+
 public class MovingPositionGenerator {
+    public boolean canMove() {
+        int randomNumber = Randoms.pickNumberInRange(START_INCULSIVE, END_INCULSIVE);
+        return randomNumber >= MOVE_POSITION_STANDARD;
+    }
 }
